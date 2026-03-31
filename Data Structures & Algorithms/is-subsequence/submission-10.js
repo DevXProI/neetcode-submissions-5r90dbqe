f2b74b50fb8d2c39b2a886652f847a7f4d9d75ff
Vikas Kr <91534659+DevXProI@users.noreply.github.com>
2026-03-31 11:45:02 +0530

    Add: is-subsequence - submission-10

---
+++ Data Structures & Algorithms/is-subsequence/submission-10.js
@@ -0,0 +1,45 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isSubsequence(s, t) {
+        // if(!(s.length <= t.length)) {
+        //     return false;
+        // }
+
+        // let j = 0;
+        // let i = 0;
+        // let isSubsequence = true;
+
+        // for(i; i < s.length; i++) {
+        //     isSubsequence = false;
+        //     for(j; j < t.length; j++) {
+        //         if(s[i] === t[j]) {
+        //             j++;
+        //             isSubsequence = true;
+        //             break;
+        //         }
+        //         isSubsequence = false;
+        //     }
+
+        //     if(!isSubsequence) {
+        //         break;
+        //     }
+        // }
+
+        // return isSubsequence;
+
+        let i = 0, j = 0;
+
+        while(i < s.length && j < t.length) {
+            if(s[i] === t[j]) {
+                i++;
+            }
+            j++;
+        }
+
+        return i === s.length;
+    }
+}
